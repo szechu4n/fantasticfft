dd0b26aa71db4b6164f3b8615bfe960a7189ad58
Andrew Sweeney <sweeneyandrewliam@gmail.com>
2022-11-01 19:53:28 -0500

    Added some basic definitions

---
+++ rtl/fantasticfft_ram.sv
@@ -0,0 +1,13 @@
+module fantasticfft_ram #(
+    parameter DEPTH = 64
+) (
+    input logic clk,
+    input logic reset,
+    input logic addr_a,
+    input logic addr_b,
+    input logic wr_a,
+    input logic wr_b
+    
+);
+    
+endmodule
