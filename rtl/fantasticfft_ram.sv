ac36c1762c79f40910686a4e3adb31d56470cc35
Kevin <60239929+C-My-Code@users.noreply.github.com>
2022-11-22 13:15:34 -0600

    Update fantasticfft_ram.sv

--- rtl/fantasticfft_ram.sv
+++ rtl/fantasticfft_ram.sv
@@ -1,33 +1,27 @@
 module ram
-#(parameter data_width = 8, data_depth = 16)
+#(parameter data_width = 16, data_depth = 8)
 (
 input logic clk, reset, read_write,
 input logic [data_depth-1:0] address,
+output logic bus_clr,
 inout [data_width-1:0] data_bus);
 typedef enum logic [1:0] {RESET, READ, WRITE} e_state;
 e_state state;
 
 logic in_out_tri;
 logic [data_width-1:0] data_buffer;
-logic [data_depth-1:0] memory [data_width-1:0];
+logic [data_width-1:0] memory [data_depth-1:0];
 
 assign data_bus = in_out_tri ? data_buffer : {data_width'('bz)};
 
 always @(posedge clk) begin
 	if(reset) begin
 		state = RESET;
-		in_out_tri = 0;
 	end
 	else begin
 		case(read_write)
-			0: begin
-				state = READ;
-				in_out_tri = 1;
-			end
-			1: begin
-				state = WRITE;
-				in_out_tri = 0;
-			end
+			0: state = READ;
+			1: state = WRITE;
 		endcase;
 	end
 end
@@ -36,13 +30,23 @@ end
 always @(posedge clk) begin
 	case(state)
 		RESET: begin
+      in_out_tri = 0;
 			for(int i=0; i<data_depth;i++) begin
 				memory[i] = {data_width'('h0)};
 			end
 		end
-		READ: data_buffer = memory[address];
-		WRITE: memory[address] = data_buffer;
+		READ: begin
+      data_buffer = memory[address];
+      bus_clr = 0;
+      in_out_tri = 1;
+    end
+		WRITE: begin
+       memory[address] = data_bus; //Last value on bus before read_write goes low gets stored
+       bus_clr = 1; //Writing module should not drive bus until bus_clr is received
+       in_out_tri = 0;
+     end
 	endcase;
 end
 
 endmodule
+
