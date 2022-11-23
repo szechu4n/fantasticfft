55fbaca46db46512ee37aa71da754db793910b30
Andrew Sweeney <sweeneyandrewliam@gmail.com>
2022-11-22 20:06:40 -0600

    Starting testbench design

--- tests/fft8_tb.sv
+++ tests/fft8_tb.sv
@@ -10,10 +10,22 @@
 module fantastic_fft8_tb (
     fantasticfft_fft8_if fft8if
 );
+
     initial begin
         logic [7:-8] thing1 = 16'b0000_0000_1011_0101;
         logic [7:-8] thing2 = `CREATE_CONSTANT_FIXED_POINT(8, 8, 3'b0, 8'b1011_0101);
-        $$display("Constant test 1: %b\tConstant test2: %b");
+        $display("-----------------------------------------------------------------");
+        $display("Testing macros...");
+        $display("\tConstant test 1: %b\tConstant test2: %b", thing1, thing2);
+
+        $assert (thing1 == thing2)
+        else $display("\tMacro failed to correctly create fixed point number.");
+
+        $display("-----------------------------------------------------------------");
+        $display("Beginning test...");
+
+        
+        
         $finish();
     end
 endmodule
