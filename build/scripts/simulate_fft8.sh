678ea0207c2a103394c1b2e2d084a4dba7feaebe
Andrew Sweeney <sweeneyandrewliam@gmail.com>
2022-11-22 19:09:48 -0600

    Fixed bash script

--- build/scripts/simulate_fft8.sh
+++ build/scripts/simulate_fft8.sh
@@ -4,5 +4,5 @@ if [ ! -d "build/f_sim" ]; then
     mkdir build/f_sim
 fi
 
-vcs -sverilog -full64 -debug_all tests/fft8_tb_top.sv rtl/fft8_if.sv rtl/fft8.sv tests/fft8_tb.sv build/f_sim/fft8.simv > build/f_sim/fft8_compilation.txt
+vcs -sverilog -full64 -debug_all tests/fft8_tb_top.sv rtl/fft8_if.sv rtl/fft8.sv tests/fft8_tb.sv -o build/f_sim/fft8.simv > build/f_sim/fft8_compilation.txt
 ./build/f_sim/fft8.simv > build/f_sim/fft8_simulation.txt
