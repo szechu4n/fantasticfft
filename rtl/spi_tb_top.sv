cf5fae51c3fca82a2d154041cf892d4ed42c67c3
Kevin <60239929+C-My-Code@users.noreply.github.com>
2022-11-22 19:37:21 -0600

    Names

---
+++ rtl/spi_tb_top.sv
@@ -0,0 +1,17 @@
+`timescale 1ns/1ps
+module spi_tb_top();
+
+logic s_clk_w, slave_sel_w, reset_w, mosi_w, miso_w, send_comp_w;
+logic [7:0] mcu_out_w, data_to_mcu_w;
+bit clk_w;
+
+always #5 clk_w=~clk_w;
+
+spi_interface si(.clk(clk_w), .s_clk(s_clk_w), .reset(reset_w), .mosi(mosi_w), .slave_sel(slave_sel_w), .data_in(mcu_out_w), .miso(miso_w), .send_complete(send_comp_w),
+.data_out(data_to_mcu_w));
+
+
+spi_interface_tb stb(.send_complete(send_comp_w), .clk(clk_w), .s_clk(s_clk_w), .slave_sel(slave_sel_w), .reset(reset_w), .master_out(mosi_w),
+.mcu_out(mcu_out_w));
+
+endmodule
