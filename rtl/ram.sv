7119b9915cd6aaa88a09f630a09ee9e911004a4d
szechuan <86889252+szechu4n@users.noreply.github.com>
2022-11-28 19:00:39 -0600

    Issue/ramming speed (#2)
    * Update fantasticfft_ram.sv
    
    * Ram & Spi w/ TBs
    
    * Update fantasticfft_ram.sv
    
    * Update fantasticfft_ram.sv
    
    * Update
    
    * Names
    
    * Ram Update
    
    * Removing old fantasticfft refs
    
    Co-authored-by: Kevin <60239929+C-My-Code@users.noreply.github.com>

---
+++ rtl/ram.sv
@@ -0,0 +1,22 @@
+module ram
+#(parameter data_width = 16, data_depth = 8)
+(
+input logic read_write, reset,
+input logic [data_depth-1:0] address,
+input logic [data_width-1:0] in_data,
+output logic [data_width-1:0] out_data);
+
+
+logic [data_width-1:0] memory [data_depth-1:0];
+
+always @(posedge read_write) begin
+  memory[address] = in_data;
+end
+
+always @(address, read_write) begin
+  if(~read_write)out_data = memory[address];
+end
+
+
+endmodule
+
