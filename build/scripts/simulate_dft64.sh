d7e2ed6dec174230aaedae61932b18ecc904ee3d
Andrew Sweeney <sweeneyandrewliam@gmail.com>
2022-12-07 22:47:45 -0600

    eff

--- build/scripts/simulate_dft64.sh
+++ build/scripts/simulate_dft64.sh
@@ -5,5 +5,5 @@ if [ ! -d "build/f_sim" ]; then
     mkdir build/f_sim
 fi
 
-vcs -sverilog -full64 -debug_all tests/dft64_tb_top.sv rtl/dft64_if.sv rtl/dft64.sv tests/dft64_tb.sv -o build/f_sim/dft64.simv > build/f_sim/dft64_compilation.txt
+vcs -sverilog -full64 -debug_all tests/dft64_tb_top.sv rtl/fixedpoint.sv rtl/fft8.sv rtl/complexmultiplier.sv rtl/sine_lut.sv rtl/dft64_if.sv rtl/dft64.sv tests/dft64_tb.sv -o build/f_sim/dft64.simv > build/f_sim/dft64_compilation.txt
 ./build/f_sim/dft64.simv > build/f_sim/dft64_simulation.txt
