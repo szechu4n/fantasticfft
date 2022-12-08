35eabddb914df1d37dd6dbeeb2141038ceebabe7
Andrew Sweeney <sweeneyandrewliam@gmail.com>
2022-12-07 20:55:29 -0600

    eff it

---
+++ build/scripts/simulate_dft64.sh
@@ -0,0 +1,9 @@
+# run from within top level directory (fantasticfft)
+
+if [ ! -d "build/f_sim" ]; then
+    echo "Creating f_sim directory..."
+    mkdir build/f_sim
+fi
+
+vcs -sverilog -full64 -debug_all tests/dft64_tb_top.sv rtl/dft64_if.sv rtl/dft64.sv tests/dft64_tb.sv -o build/f_sim/dft64.simv > build/f_sim/dft64_compilation.txt
+./build/f_sim/dft64.simv > build/f_sim/dft64_simulation.txt
